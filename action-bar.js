ab522d49bec6b324c9c6d9046ca96ac3b59f352d
albinabist1993@mail.ru <b.hapilin@gateline.ru>
2023-01-12 11:12:08 +0500

    исправление недочетов

--- action-bar.js
+++ action-bar.js
@@ -1,11 +1,28 @@
+if(window.innerWidth > 1400) {
+    window.addEventListener("scroll", () => {
+        console.log(window.scrollY);
+        if(window.scrollY >= 51) {
+            document.querySelector(".action-bar__section").classList.add("action-bar__section--fixed");
+            document.body.style.paddingTop = '101px';
+            //document.querySelector(".action-bar__call-number").style.display = "none";
+        }
+     
+        if(window.scrollY <= 50) {
+            document.querySelector(".action-bar__section").classList.remove("action-bar__section--fixed");
+            document.body.style.paddingTop = '0';
+            //document.querySelector(".action-bar__call-number").style.display = "flex";
+        }
+     });
+}
+
 window.addEventListener("scroll", () => {
-   if(window.scrollY >= 110){
-    document.querySelector(".action-bar__section").classList.add("action-bar__section--fixed");
-    document.querySelector(".action-bar__logo").classList.add("action-bar__logo--fixed");
+    if(window.scrollY >= 70) {
+        document.querySelector(".action-bar__section").classList.add("action-bar__section--fixed");
+        document.body.style.paddingTop = '80px';
     }
 
-    if(window.scrollY <= 110){
+    if(window.scrollY <= 69) {
         document.querySelector(".action-bar__section").classList.remove("action-bar__section--fixed");
-        document.querySelector(".action-bar__logo").classList.remove("action-bar__logo--fixed");
-        }
+        document.body.style.paddingTop = '0';
+    }
 });
